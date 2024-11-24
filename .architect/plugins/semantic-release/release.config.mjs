8d7d89f3eae17a0d7150fd53352f9873cbdcc1ec
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-11-24 21:51:21 +0100

    feat: integrated semantic-release task

--- .architect/plugins/semantic-release/release.config.mjs
+++ .architect/plugins/semantic-release/release.config.mjs
@@ -1,12 +1,15 @@
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
     "@semantic-release/git",
-    ["@semantic-release/github",  {
-        "assets": [{
-            "path": "build/libs/architect-cli-0.1-all.jar",
-            "name": "architect-cli.jar",
-            "label": "Architect CLI"
-        }],
+    [
+        "@semantic-release/github",  {
+        "assets": [
+            {
+                "path": "../../../architect-cli/build/libs/architect-cli-0.1-all.jar",
+                "name": "architect-cli.jar",
+                "label": "Architect CLI"
+            }
+        ],
     }],
 )
 export default config;
--- architect-cli/src/main/kotlin/io/github/alemazzo/architect/cli/setup/gradle/checks/GradleBuild.kt
+++ architect-cli/src/main/kotlin/io/github/alemazzo/architect/cli/setup/gradle/checks/GradleBuild.kt
@@ -2,15 +2,20 @@ package io.github.alemazzo.architect.cli.setup.gradle.checks
 
 import io.github.alemazzo.architect.cli.api.annotation.Plugin
 import io.github.alemazzo.architect.cli.api.check.Check
+import io.github.alemazzo.architect.cli.api.init.Initializer
 import io.github.alemazzo.architect.cli.setup.gradle.GradleExecutor
 import io.micronaut.context.annotation.Requires
 
 @Plugin
 @Requires(property = "architect.gradle.enabled", value = "true", defaultValue = "true")
-class GradleBuild(private val gradleExecutor: GradleExecutor) : Check {
+class GradleBuild(private val gradleExecutor: GradleExecutor) : Check, Initializer {
     override fun check(): Boolean {
         println("Checking if the project can be built with Gradle")
         return gradleExecutor.execute(arrayOf("build"))
+    }
+
+    override fun init(): Boolean {
+        return check()
     }
 }
 
