544c76907bf30f762efa169912dbf8c3a04e4c6b
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-11-24 21:00:35 +0100

    feat: integrated semantic-release task

--- .architect/plugins/semantic-release/release.config.mjs
+++ .architect/plugins/semantic-release/release.config.mjs
@@ -1,4 +1,6 @@
+import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
 config.plugins.push(
+
     "@semantic-release/github",
     "@semantic-release/git",
 )
