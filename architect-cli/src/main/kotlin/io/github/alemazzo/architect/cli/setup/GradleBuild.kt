5f60fde731ac0d569d7f9af248b2d3c303e4190a
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-11-24 17:59:05 +0100

    feat: added example of property based check

--- architect-cli/src/main/kotlin/io/github/alemazzo/architect/cli/setup/GradleBuild.kt
+++ architect-cli/src/main/kotlin/io/github/alemazzo/architect/cli/setup/GradleBuild.kt
@@ -3,8 +3,10 @@ package io.github.alemazzo.architect.cli.setup
 import io.github.alemazzo.architect.cli.api.annotation.Plugin
 import io.github.alemazzo.architect.cli.api.check.Check
 import io.github.alemazzo.architect.cli.setup.gradle.GradleExecutor
+import io.micronaut.context.annotation.Requires
 
 @Plugin
+@Requires(property = "architect.check.gradle.enabled", value = "true", defaultValue = "false")
 class GradleBuild(private val gradleExecutor: GradleExecutor) : Check {
     override fun check(): Boolean {
         println("Checking if the project can be built with Gradle")
