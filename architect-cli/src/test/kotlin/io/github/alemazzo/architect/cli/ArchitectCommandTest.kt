8000ea7f0aa2d6a7e87c8b27e48a0047cb3f4eaf
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2024-11-24 15:30:20 +0100

    feat: fix test

--- architect-cli/src/test/kotlin/io/github/alemazzo/architect/cli/ArchitectCommandTest.kt
+++ architect-cli/src/test/kotlin/io/github/alemazzo/architect/cli/ArchitectCommandTest.kt
@@ -5,7 +5,10 @@ import io.micronaut.configuration.picocli.PicocliRunner
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.env.Environment
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
@@ -15,17 +18,27 @@ class ArchitectCommandTest {
 
     val ctx = ApplicationContext.run(Environment.CLI, Environment.TEST)
 
-    @Test
-    fun testWithCommandLineOption() {
-
+    fun <T: Runnable> execute(clazz: Class<T>, vararg args: String): String {
         val baos = ByteArrayOutputStream()
         System.setOut(PrintStream(baos))
+        PicocliRunner.run(clazz, ctx, *args)
+        return baos.toString()
+    }
 
-        val args = arrayOf("-v")
-        PicocliRunner.run(ArchitectCommand::class.java, ctx, *args)
-
-        Assertions.assertTrue(baos.toString().contains("Hello from Architect!"))
-
+    @AfterEach
+    fun cleanup() {
         ctx.close()
+    }
+
+    @Test
+    fun testWithCheck() {
+        val output = execute(ArchitectCommand::class.java, "check")
+        Assertions.assertTrue(output.contains("Hello from Check!"))
+    }
+
+    @Test
+    fun testWithHelp() {
+        val output = execute(ArchitectCommand::class.java, "help")
+        Assertions.assertTrue(output.contains("Hello from Architect!"))
     }
 }
