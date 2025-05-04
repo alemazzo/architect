f831d945225f84814ff91c5a1da0710c0705ad44
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2025-05-04 03:33:48 +0200

    chore(installer): fixed ci installer

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -1,11 +1,15 @@
 ARCHITECT_BIN_DIR="$HOME/.local/bin"
 mkdir -p "$ARCHITECT_BIN_DIR"
-
 GITHUB_REPO="alemazzo/architect"
 ASSET_NAME="architect.jar"
 JAR_PATH="$ARCHITECT_BIN_DIR/architect.jar"
-
+WRAPPER_PATH="$ARCHITECT_BIN_DIR/architect"
 ASSET_URL=$(curl -s "https://api.github.com/repos/$GITHUB_REPO/releases/latest" | jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
 curl -s -L "$ASSET_URL" --output "$JAR_PATH"
-
-alias architect="java -jar $JAR_PATH"
+cat <<'EOF' > "$WRAPPER_PATH"
+#!/bin/bash
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+exec java -jar "$DIR/architect.jar" "$@"
+EOF
+chmod +x "$WRAPPER_PATH"
+echo "$ARCHITECT_BIN_DIR" >> $GITHUB_PATH
